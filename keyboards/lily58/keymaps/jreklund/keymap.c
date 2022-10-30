56c67dcbf338a78f02820cca8653f98ee6e547cb
Johan Eklund <hello@johaneklund.com>
2022-10-30 16:53:10 +0100

    Add numbers and ctrl/shift to qwerty and colemak-dh layer

--- keyboards/lily58/keymaps/jreklund/keymap.c
+++ keyboards/lily58/keymaps/jreklund/keymap.c
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .-----------------------------------------.                    .-----------------------------------------.
- * |L_COL |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |L_COL |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |   Å  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   A  |   S  |   D  |   F  |   G  |-------.    .-------|   H  |   J  |   K  |   L  |   '  |   Ä  |
+ * | LSFT |   A  |   S  |   D  |   F  |   G  |-------.    .-------|   H  |   J  |   K  |   L  |   '  |   Ä  |
  * |------+-LGUI-+-LALT-+-LCTL-+-LSFT-+------|       |    |       |------+-LSFT-+-LCTL-+-LALT-+-LGUI-+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |   Ö  |
+ * | LCTL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |   Ö  |
  * '---------------RALT----------------------/       /    \       \----------------------RALT---------------'
  *                   |      | ESC  | SPC  | /  TAB  /      \  ENT  \  | BSPC | DEL  |      |
  *                   |      |      |      |/       /        \       \ |      |      |      |
@@ -85,22 +85,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  L_COL,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,     KC_NO,
-  KC_NO,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,      RALT_W,
-  KC_NO,  MTQ_A,  MTQ_S,  MTQ_D,  MTQ_F,  KC_G,                     KC_H,   MTQ_J,  MTQ_K,    MTQ_L,    MTQ_QUOT,  RALT_A,
-  KC_NO,  KC_Z,   MTQ_X,  KC_C,   KC_V,   KC_B,   KC_NO,    KC_NO,  KC_N,   KC_M,   KC_COMM,  MTQ_DOT,  KC_SLSH,   RALT_O,
-                          KC_NO,  L_MIS,  L_NAV,  L_MUS,    L_SYM,  L_NUM,  L_FUN,  KC_NO
+  L_COL,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,     KC_9,     KC_0,      KC_NO,
+  KC_NO,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,      RALT_W,
+  KC_LSFT,  MTQ_A,  MTQ_S,  MTQ_D,  MTQ_F,  KC_G,                     KC_H,   MTQ_J,  MTQ_K,    MTQ_L,    MTQ_QUOT,  RALT_A,
+  KC_LCTL,  KC_Z,   MTQ_X,  KC_C,   KC_V,   KC_B,   KC_NO,    KC_NO,  KC_N,   KC_M,   KC_COMM,  MTQ_DOT,  KC_SLSH,   RALT_O,
+                            KC_NO,  L_MIS,  L_NAV,  L_MUS,    L_SYM,  L_NUM,  L_FUN,  KC_NO
 ),
 
 /* COLEMAK-DH
  * .-----------------------------------------.                    .-----------------------------------------.
- * |L_QWE |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |L_QWE |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   '  |   Å  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   A  |   R  |   S  |   T  |   G  |-------.    .-------|   M  |   N  |   E  |   I  |   O  |   Ä  |
+ * | LSFT |   A  |   R  |   S  |   T  |   G  |-------.    .-------|   M  |   N  |   E  |   I  |   O  |   Ä  |
  * |------+-LGUI-+-LALT-+-LCTL-+-LSFT-+------|       |    |       |------+-LSFT-+-LCTL-+-LALT-+-LGUI-+------|
- * |      |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |   Ö  |
+ * | LCTL |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |   Ö  |
  * '---------------RALT----------------------/       /    \       \----------------------RALT---------------'
  *                   |      | ESC  | SPC  | /  TAB  /      \  ENT  \  | BSPC | DEL  |      |
  *                   |      |      |      |/       /        \       \ |      |      |      |
@@ -108,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT(
-  L_QWE,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
-  KC_NO,  KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                     KC_J,   KC_L,   KC_U,     KC_Y,     KC_QUOT,  RALT_W,
-  KC_NO,  MTC_A,  MTC_R,  MTC_S,  MTC_T,  KC_G,                     KC_M,   MTC_N,  MTC_E,    MTC_I,    MTC_O,    RALT_A,
-  KC_NO,  KC_Z,   MTC_X,  KC_C,   KC_D,   KC_V,   KC_NO,    KC_NO,  KC_K,   KC_H,   KC_COMM,  MTC_DOT,  KC_SLSH,  RALT_O,
-                          KC_NO,  L_MIS,  L_NAV,  L_MUS,    L_SYM,  L_NUM,  L_FUN,  KC_NO
+  L_QWE,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,     KC_9,     KC_0,     KC_NO,
+  KC_NO,    KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                     KC_J,   KC_L,   KC_U,     KC_Y,     KC_QUOT,  RALT_W,
+  KC_LSFT,  MTC_A,  MTC_R,  MTC_S,  MTC_T,  KC_G,                     KC_M,   MTC_N,  MTC_E,    MTC_I,    MTC_O,    RALT_A,
+  KC_LCTL,  KC_Z,   MTC_X,  KC_C,   KC_D,   KC_V,   KC_NO,    KC_NO,  KC_K,   KC_H,   KC_COMM,  MTC_DOT,  KC_SLSH,  RALT_O,
+                            KC_NO,  L_MIS,  L_NAV,  L_MUS,    L_SYM,  L_NUM,  L_FUN,  KC_NO
 ),
 
 /* MISC
